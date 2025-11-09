6bc97442d41c4136dcc689a20e498f8a4c357c6f
paplauskis <paplauskaskostas@gmail.com>
2025-11-09 16:28:29 +0200

    implement GetReviews method

--- internal/wifi/handler.go
+++ internal/wifi/handler.go
@@ -2,6 +2,8 @@ package wifi
 
 import (
 	"database/sql"
+	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
@@ -15,7 +17,21 @@ func NewHandler(db *sql.DB) *Handler {
 }
 
 func (h *Handler) GetReviews(context *gin.Context) {
+	id := context.Query("id")
 
+	parsedId, err := strconv.ParseInt(id, 10, 64)
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, "Invalid ID format.")
+	}
+
+	wifiReviews, err := QueryWifiReview(parsedId, h.db)
+
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, err.Error())
+	}
+
+	context.JSON(http.StatusOK, wifiReviews)
 }
 
 func (h *Handler) CreateReview(context *gin.Context) {
