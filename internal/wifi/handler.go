4901f5a23dcd47c935ff725ab04787105bcd04a2
paplauskis <paplauskaskostas@gmail.com>
2025-11-09 12:43:44 +0200

    create handler and method signatures

--- internal/wifi/handler.go
+++ internal/wifi/handler.go
@@ -1,1 +1,31 @@
 package wifi
+
+import (
+	"database/sql"
+
+	"github.com/gin-gonic/gin"
+)
+
+type Handler struct {
+	db *sql.DB
+}
+
+func NewHandler(db *sql.DB) *Handler {
+	return &Handler{db: db}
+}
+
+func (h *Handler) GetReviews(context *gin.Context) {
+
+}
+
+func (h *Handler) CreateReview(context *gin.Context) {
+
+}
+
+func (h *Handler) GetPasswords(context *gin.Context) {
+
+}
+
+func (h *Handler) AddPassword(context *gin.Context) {
+
+}
