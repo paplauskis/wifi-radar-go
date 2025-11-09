c2844f7033d2f2a12d65d9ce6dc725222348c25f
paplauskis <paplauskaskostas@gmail.com>
2025-11-09 16:24:04 +0200

    add new query for retrieving wifi info by ID
    multiple wifis can be fetched with multiple IDs specified in argument array

--- internal/external/overpass/queries.go
+++ internal/external/overpass/queries.go
@@ -1,6 +1,10 @@
 package overpass
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"strings"
+)
 
 func FreeWifiInCity(city string) string {
 	return `[out:json];
@@ -29,4 +33,23 @@ node
   ["addr:street"="` + street + `"]
   ["addr:housenumber"="` + buildingNumber + `"];
 out body;`
+}
+
+func WifisByIDs(ids []int64) (string, error) {
+	if len(ids) == 0 {
+		return "", errors.New("No IDs provided")
+	}
+	var idString strings.Builder
+
+	for i := 0; i < len(ids); i++ {
+		if i != len(ids)-1 {
+			idString.WriteString(fmt.Sprintf("%d, ", ids[i]))
+		} else {
+			idString.WriteString(fmt.Sprintf("%d", ids[i]))
+		}
+	}
+
+	return `[out:json];
+node(` + idString.String() + `)
+out body;`, nil
 }
