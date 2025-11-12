d39c73b682a115fe6c6ddadcbd0a371fd2eceb92
Šarlotė Volosevič <s053735@ad.viko.lt>
2025-11-12 16:23:06 +0200

    Added routes for user

--- internal/router/router.go
+++ internal/router/router.go
@@ -3,7 +3,8 @@ package router
 import (
 	"database/sql"
 	_map "wifi-radar-go/internal/map"
-	_wifi "wifi-radar-go/internal/wifi"
+	"wifi-radar-go/internal/middleware"
+	"wifi-radar-go/internal/user"
 
 	"github.com/gin-gonic/gin"
 )
@@ -12,33 +13,41 @@ func SetupRoutes(db *sql.DB) *gin.Engine {
 	r := gin.Default()
 
 	mapHandler := _map.NewHandler(db)
-	//userHandler := user.NewHandler(db)
-	wifiHandler := _wifi.NewHandler(db)
+	userHandler := user.NewUserHandler(db)
+	//wifiHandler := wifi.NewHandler(db)
 
 	api := r.Group("/api")
 	{
+		// Map routes
 		mapRoutes := api.Group("/map")
 		{
 			mapRoutes.GET("/search", mapHandler.Search)
 			mapRoutes.GET("/coordinates", mapHandler.GetCoordinates)
 		}
 
-		//userRoutes := api.Group("/user")
-		//{
-		//	userRoutes.POST("/auth/login", userHandler.Login)
-		//	userRoutes.POST("/auth/register", userHandler.Register)
-		//	userRoutes.POST("/:userId/favorites", userHandler.AddFavorite)
-		//	userRoutes.GET("/:userId/favorites", userHandler.GetFavorites)
-		//	userRoutes.DELETE("/:userId/favorites/:wifiId", userHandler.DeleteFavorite)
-		//}
-
-		wifiRoutes := api.Group("/wifi")
+		// User routes
+		userRoutes := api.Group("/user")
 		{
-			wifiRoutes.GET("/reviews", wifiHandler.GetReviews)
-			wifiRoutes.POST("/reviews", wifiHandler.CreateReview)
-			wifiRoutes.GET("/passwords", wifiHandler.GetPasswords)
-			wifiRoutes.POST("/passwords", wifiHandler.AddPassword)
+			userRoutes.POST("/auth/login", userHandler.Login)
+			userRoutes.POST("/auth/register", userHandler.Register)
+
+			protected := userRoutes.Group("/")
+			protected.Use(middleware.AuthenticationMiddleware())
+			{
+				protected.POST("/:id/favorite", middleware.OwnData(), userHandler.AddFavorite)
+				protected.GET("/:id/favorite", middleware.OwnData(), userHandler.GetFavorite)
+				protected.DELETE("/:id/favorite/:wifi_id", middleware.OwnData(), userHandler.DeleteFavorite)
+
+			}
 		}
+
+		//wifiRoutes := api.Group("/wifi")
+		//{
+		//	wifiRoutes.POST("/reviews", wifiHandler.CreateReview)
+		//	wifiRoutes.GET("/reviews", wifiHandler.GetReviews)
+		//	wifiRoutes.POST("/passwords", wifiHandler.AddPassword)
+		//	wifiRoutes.GET("/passwords", wifiHandler.GetPasswords)
+		//}
 	}
 
 	return r
